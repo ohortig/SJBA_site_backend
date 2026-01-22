2de622ceee180675526b8b1872915c85ef519c23
Omer Hortig <omer.hortig@gmail.com>
2026-01-22 16:04:49 -0500

    Return board member data in camelCase

--- types/boardMember.ts
+++ types/boardMember.ts
@@ -11,31 +11,3 @@ export interface BoardMemberRow {
      headshot_file: string | null;
      order_index: number;
 }
-
-export interface BoardMemberData {
-     id: string;
-     full_name: string;
-     position: string;
-     bio: string;
-     major: string;
-     year: string;
-     hometown: string;
-     linkedin_url: string | null;
-     email: string;
-     headshot_file: string | null;
-     order_index: number;
-}
-
-export interface BoardMemberJSON {
-     id: string;
-     full_name: string;
-     position: string;
-     bio: string;
-     major: string;
-     year: string;
-     hometown: string;
-     linkedin_url: string | null;
-     email: string;
-     headshot_file: string | null;
-     order_index: number;
-}
