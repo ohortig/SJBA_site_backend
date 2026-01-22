40b2407faf789f877ea79a005bcea5465bf02b92
Omer Hortig <omer.hortig@gmail.com>
2026-01-22 16:05:10 -0500

    Standardize contact form types

--- types/contact.ts
+++ types/contact.ts
@@ -8,22 +8,12 @@ export interface ContactSubmissionRow {
      created_at: string;
 }
 
-export interface ContactFormData {
-     id?: string;
-     first_name?: string;
-     last_name?: string;
-     email?: string;
-     company?: string | null;
-     message?: string;
-     created_at?: string;
-}
-
 export interface ContactFormJSON {
-     id: string | undefined;
-     firstName: string | undefined;
-     lastName: string | undefined;
-     email: string | undefined;
-     company: string | null | undefined;
-     message: string | undefined;
+     id: string;
+     firstName: string;
+     lastName: string;
+     email: string;
+     company: string | null;
+     message: string;
      createdAt: string;
 }
